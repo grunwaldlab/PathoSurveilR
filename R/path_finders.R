a0e67a30a7947091624e6c9f09709279dc7f9e09
Zachary Foster <fosterz@pop-os.localdomain>
2025-05-23 17:51:59 -0700

    added functions to show available output data

--- R/path_finders.R
+++ R/path_finders.R
@@ -97,17 +97,22 @@ find_path_data <- function(outdir_path, targets = NULL, simplify = TRUE, long = 
     target_meta <- metadata$outputs[[target]]
     do.call(rbind, lapply(target_meta$sources, function(source) {
       source_path <- file.path(outdir_path, source$path)
-      possible_paths <- list.files(path = source_path, recursive = TRUE)
+      possible_paths <- list.files(path = source_path, recursive = TRUE,
+                                   all.files = TRUE, include.dirs = TRUE)
       matching_paths <- possible_paths[grepl(possible_paths, pattern = source$pattern)]
       matching_paths <- matching_paths[file.exists(file.path(source_path, matching_paths))]
       if (length(matching_paths) == 0) {
         return(NULL)
       }
-      capture_group_key <- rep(list(character(0)), length(source$capture_groups))
-      names(capture_group_key) <- source$capture_groups
-      output <- utils::strcapture(matching_paths, pattern = source$pattern, proto = capture_group_key)
+      if (is.null(source$capture_groups)) {
+        output <- tibble::tibble(path = file.path(source_path, matching_paths))
+      } else {
+        capture_group_key <- rep(list(character(0)), length(source$capture_groups))
+        names(capture_group_key) <- source$capture_groups
+        output <- utils::strcapture(matching_paths, pattern = source$pattern, proto = capture_group_key)
+        output$path <- file.path(source_path, matching_paths)
+      }
       output[names(source$constants)] <- source$constants
-      output$path <- file.path(source_path, matching_paths)
       tibble::as_tibble(output)
     }))
   })
@@ -145,6 +150,85 @@ find_path_data <- function(outdir_path, targets = NULL, simplify = TRUE, long = 
   }
 
   return(path_data)
+}
+
+
+#' Prints available pipeline output files
+#'
+#' Prints available pipeline output files grouped by type
+#'
+#' @inheritParams known_outputs
+#'
+#' @return Invisibly returns a nested list of categories with their items and descriptions.
+#'   Primarily called for its side effect of printing formatted output to the console.
+#'
+#' @export
+print_known_outputs <- function(outdir_path, exists = FALSE) {
+  desc_data <- known_outputs(outdir_path, exists = exists)
+
+  # Get print text for each line
+  desc_data$print_text <- paste0(
+    '  ',
+    format(desc_data$key, width = max(nchar(desc_data$key)), justify = "left"),
+    ' : ',
+    desc_data$description
+  )
+  
+  # Print results grouped by category
+  category_print_text <- lapply(split(desc_data, desc_data$category), function(category_data) {
+    category <- unique(category_data$category)
+    width <- max(nchar(desc_data$print_text))
+    header <- paste0(
+      format(category, width = width, justify = 'left'), '\n',
+      format(strrep("\U00AF", width), width = width, justify = 'left')
+    )
+    paste0(c(header, category_data$print_text), collapse = '\n')
+  })
+  cat(paste0(category_print_text, collapse = '\n\n'))
+}
+
+
+#' Get which outputs are available
+#'
+#' Get which outputs are available for a given pathogensurveillance output
+#' directory.
+#'
+#' @param outdir_path The path to an output directory of pathogensurviellance
+#' @param exists If `TRUE`, information is only returned for output type that
+#'   exist, rather than all output type that are known.
+#'
+#' @export
+known_outputs <- function(outdir_path, exists = FALSE) {
+  # Load output schema metadata
+  metadata <- parse_output_meta_json(outdir_path)
+  
+  # Filter by output type that have file associated with them
+  if (exists) {
+    all_path_data <- find_path_data(outdir_path, simplify = TRUE, long = TRUE)
+    all_output_types <- unique(all_path_data$target)
+    metadata$outputs <- metadata$outputs[names(metadata$outputs) %in% all_output_types]
+  }
+  
+  # Organize outputs by category
+  desc_data <- do.call(rbind, lapply(names(metadata$outputs), function(key) {
+    item <- metadata$outputs[[key]]
+    category <- item$category
+    description <- item$description
+    if (is.null(category) || category == "") {
+      category <- 'Miscellaneous'
+    }
+    if (is.null(description)) {
+      category <- ''
+    }
+    data.frame(
+      key = key,
+      description = description,
+      category = category,
+      stringsAsFactors = FALSE
+    )
+  }))
+  
+  tibble::as_tibble(desc_data)
 }
 
 
